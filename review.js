058ac4a0ac2ad89d4f11a3c7722d33a3e98dd2c4
dwivediji425 <dwivediji425@gmail.com>
2025-08-16 04:53:50 +0530

    test again

--- review.js
+++ review.js
@@ -1,9 +1,14 @@
 import fs from "fs";
 import path from "path";
 import { execSync } from "child_process";
+import { get } from "http";
 
-getChangesFromLastCommit = () => {
-  const changes = execSync(
-    `git diff-tree --no-commit-id --name-only -r HEAD`
-  ).split("\n");
+const getChangesFromLastCommit = async () => {
+  const changes = execSync(`git diff-tree --no-commit-id --name-only -r HEAD`)
+    .toString()
+    .split("\n")
+    .filter(Boolean);
+
+  console.log(changes); // This will log the array of changed files
 };
+getChangesFromLastCommit();
